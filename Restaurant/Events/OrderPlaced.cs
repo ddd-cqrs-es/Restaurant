d57b86d9415355e213aeeb7435a395434ac8cf77
npryntsova <npryntsova@gmail.com>
2016-10-25 16:40:09 +0100

    ttl

--- Restaurant/Events/OrderPlaced.cs
+++ Restaurant/Events/OrderPlaced.cs
@@ -1,10 +1,20 @@
+using System;
+using Restaurant.Infrastructure;
 using Restaurant.Models;
 
 namespace Restaurant.Events
 {
-    public class OrderPlaced : Event
+    public class OrderPlaced : Event, ITTLMessage
     {
         public Order Order { get; set; }
+
+        public DateTime ShoulBeProcessesdBefore
+        {
+            get
+            {
+                return ((ITTLMessage)Order).ShoulBeProcessesdBefore;
+            }
+        }
 
         public OrderPlaced(Order order)
         {
